6b05038bbde991d0882234c1b395e0e4c56d19e7
yuping-xiao <yupingxiao94@gmail.com>
2020-07-27 13:12:02 +0900

    fixed script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(function(){
 	$('#btn').on('click', function() {
 		$.ajax({
-			url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + 3692353ed19cf2eb0ded13a6135a8be3,
+			url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "3692353ed19cf2eb0ded13a6135a8be3",
 			dataType : 'jsonp',
 		}).done(function (data){
-			//通信成功
 			$('#place').text(data.name);
 			$('#temp_max').text(data.main.temp_max);
 			$('#temp_min').text(data.main.temp_min);
@@ -14,8 +13,7 @@ $(function(){
 			$('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 			$('img').attr("alt",data.weather[0].main);
 
-		}).fail(function (data){
-			//通信失敗
+		}).fail(function (data) {
 			alert('通信に失敗しました。');
 		});
 	});
